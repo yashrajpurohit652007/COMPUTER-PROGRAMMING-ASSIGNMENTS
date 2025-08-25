81837ce0498eb79da75e37d643f6ffeb43445b57
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2025-08-26 00:41:07 +0530

    Division of two numbers.c

---
+++ Division of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    int division;
+    
+    printf("enter the first number: ");
+    scanf("%d", &a);
+    
+    printf("enter the second number: ");
+    scanf("%d", &b);
+    
+division=a/b;
+
+printf("division of two number is: %d\n", division);
+    return 0;
+}
