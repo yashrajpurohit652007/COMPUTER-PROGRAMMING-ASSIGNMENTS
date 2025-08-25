feb7a69f6db5da6c6f9ba2134b88a55dc1d64e8f
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2025-08-26 00:38:42 +0530

    Difference of two numbers.c

---
+++ Difference of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    int difference;
+    
+    printf("enter the first number: ");
+    scanf("%d", &a);
+    
+    printf("enter the second number: ");
+    scanf("%d", &b);
+    
+difference=a-b;
+
+printf("difference of two number is: %d\n", difference);
+    return 0;
+}
