aea8fa1fbae3253562c090aa2c7e1a8e445f360a
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2025-09-02 00:52:04 +0530

    Create  Check divisibility by 7.c

---
+++ Check divisibility by 7.c
@@ -0,0 +1,9 @@
+// Problem 4: Check divisibility by 7
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n%7==0) printf("Divisible");
+    else printf("Not Divisible");
+    return 0;
+}
