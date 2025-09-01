e86c14745220376d8904f73e345be1b505fef7c6
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2025-09-02 00:47:07 +0530

    Create Area & Perimeter of Rectangle.c

---
+++ Area & Perimeter of Rectangle.c
@@ -0,0 +1,8 @@
+// Problem 18: Area & Perimeter of Rectangle
+#include <stdio.h>
+int main() {
+    int l,b;
+    scanf("%d%d",&l,&b);
+    printf("Area=%d Perimeter=%d", l*b, 2*(l+b));
+    return 0;
+}
